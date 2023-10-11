07dec2fbbfbce4d2dccee623dbf04a4ca1f5655d
merouaniadh <merouaniadh@gmail.com>
2023-10-11 14:17:38 +0200

    Icon launcher edit

--- lib/View/HomePages/UserPages/profilepage.dart
+++ lib/View/HomePages/UserPages/profilepage.dart
@@ -20,219 +20,211 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        Get.to(ProfileScreen());
-        return Future.delayed(Duration(microseconds: 100));
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () => Get.toNamed("/homepage"),
-              icon: const Icon(
-                Iconsax.arrow_left,
-                color: Colors.black,
-              )),
-          title: Text(getStatment("Profile"), style: Get.textTheme.titleLarge),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                /// -- IMAGE
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 120,
-                      height: 120,
-                      child: GetBuilder<Authcontroller>(builder: (controller) {
-                        return ClipRRect(
-                            borderRadius: BorderRadius.circular(100),
-                            child: authcontroller.photoIsLoading == true
-                                ? const Center(
-                                    child: Loader(),
-                                  )
-                                : Image(
-                                    image: NetworkImage(
-                                      authcontroller.profile!.image!,
-                                    ),
-                                    fit: BoxFit.cover,
-                                  ));
-                      }),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                          width: 35,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            color: maincolorlighter,
-                          ),
-                          child: Image.asset("assets/logo.png")),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Text(authcontroller.profile!.name!,
-                    style: Get.textTheme.titleLarge),
-
-                const SizedBox(height: 20),
-
-                /// -- BUTTON
-
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      await authcontroller.updatephoto(false);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: maincolordarker,
-                        side: BorderSide.none,
-                        shape: const StadiumBorder()),
-                    child: Text(getStatment('Update picture'),
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                ),
-
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Get.to(MessegePage2());
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromARGB(255, 49, 214, 255),
-                        side: BorderSide.none,
-                        shape: const StadiumBorder()),
-                    child: Text(getStatment('Drivers'),
-                        style: TextStyle(color: maincolordarker)),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                const Divider(),
-                const SizedBox(height: 10),
-
-                ListTile(
-                  onTap: () {},
-                  leading: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: Colors.grey.withOpacity(0.1),
-                    ),
-                    child: Icon(Iconsax.language_circle, color: Colors.grey),
-                  ),
-                  title: Text(getStatment("Language"),
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          ?.apply(color: Colors.black)),
-                  trailing: DropdownButton<String>(
-                      elevation: 10,
-                      value: language,
-                      items: [
-                        DropdownMenuItem<String>(
-                          value: "ar",
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("العربية"),
-                              SizedBox(width: 5),
-                              SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: Image.asset("assets/algeria.png")),
-                            ],
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text("Francais"),
-                              SizedBox(width: 5),
-                              SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: Image.asset("assets/france.png")),
-                            ],
-                          ),
-                          value: "fr",
-                        ),
-                        DropdownMenuItem(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("English"),
-                              SizedBox(width: 5),
-                              SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child:
-                                      Image.asset("assets/united-kingdom.png")),
-                            ],
-                          ),
-                          value: "en",
-                        )
-                      ],
-                      onChanged: (value) {
-                        print(value);
-                        setState(() {
-                          language = value!;
-                        });
-                        changeLanguage(language);
-
-                        print(language);
-                      }),
-                ),
-                ProfileMenuWidget(
-                    title: getStatment("Biling details"),
-                    icon: Iconsax.wallet,
-                    onPress: () {
-                      Get.snackbar('Billing comming soon',
-                          'We are currently working on it thank you for your patient');
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () => Get.toNamed("/homepage"),
+            icon: const Icon(
+              Iconsax.arrow_left,
+              color: Colors.black,
+            )),
+        title: Text(getStatment("Profile"), style: Get.textTheme.titleLarge),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              /// -- IMAGE
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: GetBuilder<Authcontroller>(builder: (controller) {
+                      return ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: authcontroller.photoIsLoading == true
+                              ? const Center(
+                                  child: Loader(),
+                                )
+                              : Image(
+                                  image: NetworkImage(
+                                    authcontroller.profile!.image!,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ));
                     }),
-
-                const Divider(),
-                const SizedBox(height: 10),
-
-                ProfileMenuWidget(
-                    title: getStatment("Log out"),
-                    icon: Iconsax.logout,
-                    textColor: Colors.red,
-                    endIcon: false,
-                    onPress: () {
-                      Get.defaultDialog(
-                        title: getStatment("Log out"),
-                        titleStyle: const TextStyle(fontSize: 20),
-                        content: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 15.0),
-                          child: Text(
-                              "${getStatment('Êtes-vous sûr de vouloir vous déconnecter?')}"),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: maincolorlighter,
                         ),
-                        confirm: ElevatedButton(
-                          onPressed: () {
-                            print('logout');
-                            authcontroller.logout();
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.redAccent,
-                              side: BorderSide.none),
-                          child: const Text("Yes"),
+                        child: Image.asset("assets/logo.png")),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(authcontroller.profile!.name!,
+                  style: Get.textTheme.titleLarge),
+
+              const SizedBox(height: 20),
+
+              /// -- BUTTON
+
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await authcontroller.updatephoto(false);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: maincolordarker,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: Text(getStatment('Update picture'),
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.to(MessegePage2());
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 49, 214, 255),
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: Text(getStatment('Drivers'),
+                      style: TextStyle(color: maincolordarker)),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Divider(),
+              const SizedBox(height: 10),
+
+              ListTile(
+                onTap: () {},
+                leading: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.grey.withOpacity(0.1),
+                  ),
+                  child: Icon(Iconsax.language_circle, color: Colors.grey),
+                ),
+                title: Text(getStatment("Language"),
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        ?.apply(color: Colors.black)),
+                trailing: DropdownButton<String>(
+                    elevation: 10,
+                    value: language,
+                    items: [
+                      DropdownMenuItem<String>(
+                        value: "ar",
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("العربية"),
+                            SizedBox(width: 5),
+                            SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: Image.asset("assets/algeria.png")),
+                          ],
                         ),
-                        cancel: OutlinedButton(
-                            onPressed: () => Get.back(),
-                            child: const Text("No")),
-                      );
+                      ),
+                      DropdownMenuItem(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("Francais"),
+                            SizedBox(width: 5),
+                            SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: Image.asset("assets/france.png")),
+                          ],
+                        ),
+                        value: "fr",
+                      ),
+                      DropdownMenuItem(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("English"),
+                            SizedBox(width: 5),
+                            SizedBox(
+                                height: 20,
+                                width: 20,
+                                child:
+                                    Image.asset("assets/united-kingdom.png")),
+                          ],
+                        ),
+                        value: "en",
+                      )
+                    ],
+                    onChanged: (value) {
+                      print(value);
+                      setState(() {
+                        language = value!;
+                      });
+                      changeLanguage(language);
+
+                      print(language);
                     }),
-              ],
-            ),
+              ),
+              ProfileMenuWidget(
+                  title: getStatment("Biling details"),
+                  icon: Iconsax.wallet,
+                  onPress: () {
+                    Get.snackbar('Billing comming soon',
+                        'We are currently working on it thank you for your patient');
+                  }),
+
+              const Divider(),
+              const SizedBox(height: 10),
+
+              ProfileMenuWidget(
+                  title: getStatment("Log out"),
+                  icon: Iconsax.logout,
+                  textColor: Colors.red,
+                  endIcon: false,
+                  onPress: () {
+                    Get.defaultDialog(
+                      title: getStatment("Log out"),
+                      titleStyle: const TextStyle(fontSize: 20),
+                      content: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        child: Text(
+                            "${getStatment('Êtes-vous sûr de vouloir vous déconnecter?')}"),
+                      ),
+                      confirm: ElevatedButton(
+                        onPressed: () {
+                          print('logout');
+                          authcontroller.logout();
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.redAccent,
+                            side: BorderSide.none),
+                        child: const Text("Yes"),
+                      ),
+                      cancel: OutlinedButton(
+                          onPressed: () => Get.back(), child: const Text("No")),
+                    );
+                  }),
+            ],
           ),
         ),
       ),
